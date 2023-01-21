9232ecce4247ded8ef1996b3a673b5a37d597fa6
davidannebicque <david.annebicque@gmail.com>
2023-01-21 20:44:47 +0100

    feat: Site

---
+++ src/Form/SiteType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etablissement;
+use App\Entity\Site;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle', TextType::class, [
+                'label' => 'Libellé du site',
+            ])
+            ->add('etablissement', EntityType::class, [
+                'class' => Etablissement::class,
+                'choice_label' => 'libelle',
+                'label' => 'Etablissement',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Site::class,
+        ]);
+    }
+}
