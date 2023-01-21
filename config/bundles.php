286968910886d74655a1cd52a9adf23de4283cac
davidannebicque <david.annebicque@gmail.com>
2023-01-21 21:27:06 +0100

    feat: Fixtures des utilisateurs

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/DataFixtures/UsersFixtures.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+
+class UsersFixtures extends Fixture
+{
+    //constructeur avec password
+    public function __construct(private readonly UserPasswordHasherInterface $encoder)
+    {
+    }
+
+
+    public function load(ObjectManager $manager): void
+    {
+        //admin
+        $userAdmin = new User();
+        $userAdmin->setEmail('admin@mail.com');
+        $userAdmin->setUsername('admin');
+        $userAdmin->setNom('Admin');
+        $userAdmin->setPrenom('John');
+        $userAdmin->setRoles(['ROLE_ADMINISTRATION']);
+        $password = $this->encoder->hashPassword($userAdmin, 'test');
+        $userAdmin->setPassword($password);
+        $manager->persist($userAdmin);
+
+        //SES
+        $userSes = new User();
+        $userSes->setEmail('ses@mail.com');
+        $userSes->setUsername('ses');
+        $userSes->setNom('Scolarité');
+        $userSes->setPrenom('John');
+        $userSes->setRoles(['ROLE_SES']);
+        $password = $this->encoder->hashPassword($userSes, 'test');
+        $userSes->setPassword($password);
+        $manager->persist($userSes);
+
+        //VP
+        $userVp = new User();
+        $userVp->setEmail('vp@mail.com');
+        $userVp->setUsername('vp');
+        $userVp->setNom('Vice');
+        $userVp->setPrenom('John');
+        $userVp->setRoles(['ROLE_VP']);
+        $password = $this->encoder->hashPassword($userVp, 'test');
+        $userVp->setPassword($password);
+        $manager->persist($userVp);
+
+        //Responsable DPE Composante
+        $userDpe = new User();
+        $userDpe->setEmail('resp-dpe@mail.com');
+        $userDpe->setUsername('resp-dpe');
+        $userDpe->setNom('Responsable');
+        $userDpe->setPrenom('John');
+        $userDpe->setRoles(['ROLE_RESP_DPE']);
+        $password = $this->encoder->hashPassword($userDpe, 'test');
+        $userDpe->setPassword($password);
+        $manager->persist($userDpe);
+
+        //Responsable Formation
+        $userRespFormation = new User();
+        $userRespFormation->setEmail('formation@mail.com');
+        $userRespFormation->setUsername('formation');
+        $userRespFormation->setNom('Formation');
+        $userRespFormation->setPrenom('John');
+        $userRespFormation->setRoles(['ROLE_RESP_FORMATION']);
+        $password = $this->encoder->hashPassword($userRespFormation, 'test');
+        $userRespFormation->setPassword($password);
+        $manager->persist($userRespFormation);
+
+        //Responsable EC
+        $userRespFormation = new User();
+        $userRespFormation->setEmail('resp-ec@mail.com');
+        $userRespFormation->setUsername('resp-ec');
+        $userRespFormation->setNom('Element');
+        $userRespFormation->setPrenom('John');
+        $userRespFormation->setRoles(['ROLE_RESP_EC']);
+        $password = $this->encoder->hashPassword($userRespFormation, 'test');
+        $userRespFormation->setPassword($password);
+        $manager->persist($userRespFormation);
+
+        $manager->flush();
+    }
+}
