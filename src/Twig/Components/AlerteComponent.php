43e4e2173aefb71ebb99587b4077add27ef1df23
davidannebicque <david.annebicque@gmail.com>
2023-03-07 20:59:50 +0100

    feat: Componnent Alerte et CSS

---
+++ src/Twig/Components/AlerteComponent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Repository\NotificationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('alerte')]
+final class AlerteComponent extends AbstractController
+{
+    public string $type = 'info';
+    public string $message = 'message';
+
+    public function getIcone(): string
+    {
+        return match ($this->type) {
+            'info' => 'fa-info-circle',
+            'success' => 'fa-check-circle',
+            'warning' => 'fa-exclamation-circle',
+            'danger' => 'fas fa-times-circle',
+        };
+    }
+}
