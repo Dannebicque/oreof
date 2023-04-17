51f71e83e769a69c9850376448099a9a86cf3578
davidannebicque <david.annebicque@gmail.com>
2023-04-17 16:10:18 +0200

    feat: Filtres

--- src/Repository/FicheMatiereMutualisableRepository.php
+++ src/Repository/FicheMatiereMutualisableRepository.php
@@ -2,7 +2,11 @@
 
 namespace App\Repository;
 
+use App\Entity\Composante;
 use App\Entity\FicheMatiereMutualisable;
+use App\Entity\Formation;
+use App\Entity\Mention;
+use App\Entity\Parcours;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -50,7 +54,41 @@ class FicheMatiereMutualisableRepository extends ServiceEntityRepository
         }
 
         foreach ($options as $sort => $direction) {
-            $qb->addOrderBy('fm.' . $sort, $direction);
+            if ($sort === 'mention') {
+                $qb->leftJoin(Parcours::class, 'p', 'WITH', 'fm.parcours = p.id')
+                    ->leftJoin(Formation::class, 'fo', 'WITH', 'p.formation = fo.id')
+                    ->leftJoin(Mention::class, 'm', 'WITH', 'fo.mention = m.id')
+                    ->addOrderBy(
+                        'CASE
+                            WHEN fo.mention IS NOT NULL THEN m.libelle
+                            WHEN fo.mentionTexte IS NOT NULL THEN fo.mentionTexte
+                            ELSE fo.mentionTexte
+                            END',
+                        $direction
+                    );
+            } elseif ($sort === 'composante') {
+                $qb->leftJoin(Parcours::class, 'p', 'WITH', 'f.parcours = p.id')
+                    ->innerJoin(Formation::class, 'fo', 'WITH', 'p.formation = fo.id')
+                    ->innerJoin(Composante::class, 'co', 'WITH', 'fo.composantePorteuse = co.id')
+                    ->addOrderBy('co.libelle', $direction);
+            } elseif ($sort === 'mentionmutualisable') {
+                $qb->leftJoin(Parcours::class, 'p', 'WITH', 'f.parcours = p.id')
+                    ->leftJoin(Formation::class, 'fo', 'WITH', 'p.formation = fo.id')
+                    ->leftJoin(Mention::class, 'm', 'WITH', 'fo.mention = m.id')
+                    ->addOrderBy(
+                        'CASE
+                            WHEN fo.mention IS NOT NULL THEN m.libelle
+                            WHEN fo.mentionTexte IS NOT NULL THEN fo.mentionTexte
+                            ELSE fo.mentionTexte
+                            END',
+                        $direction
+                    );
+            }elseif ($sort === 'parcours') {
+                $qb->leftJoin(Parcours::class, 'p', 'WITH', 'f.parcours = p.id')
+                    ->addOrderBy('p.libelle', $direction);
+            } else {
+                $qb->addOrderBy('fm.' . $sort, $direction);
+            }
         }
 
         return $qb->getQuery()->getResult();
