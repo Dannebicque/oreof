adde7d3c13a8a02aa6b24cf0511cc1e212ef94c6
davidannebicque <david.annebicque@gmail.com>
2023-05-31 21:59:05 +0200

    feat: Tri du tableau des droits

--- src/Repository/RoleRepository.php
+++ src/Repository/RoleRepository.php
@@ -18,7 +18,6 @@ use Doctrine\Persistence\ManagerRegistry;
  *
  * @method Role|null find($id, $lockMode = null, $lockVersion = null)
  * @method Role|null findOneBy(array $criteria, array $orderBy = null)
- * @method Role[]    findAll()
  * @method Role[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class RoleRepository extends ServiceEntityRepository
@@ -26,6 +25,11 @@ class RoleRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Role::class);
+    }
+
+    public function findAll(): array
+    {
+        return $this->findBy([], ['libelle' => 'ASC']);
     }
 
     public function save(Role $entity, bool $flush = false): void
