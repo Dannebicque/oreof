c9b2a253a2bf64f2572361bb7d97cf87f6538a3d
davidannebicque <david.annebicque@gmail.com>
2023-07-03 09:25:58 +0200

    feat: Ajout d'une page de documentation

---
+++ src/Controller/DocumentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DocumentController extends AbstractController
+{
+    #[Route('/documents', name: 'app_document')]
+    public function index(): Response
+    {
+        return $this->render('document/index.html.twig', [
+
+        ]);
+    }
+}
