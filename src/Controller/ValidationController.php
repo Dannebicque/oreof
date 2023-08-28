33d8bf865cfac0e290741355509001701ca1c4a1
davidannebicque <david.annebicque@gmail.com>
2023-08-28 22:29:52 +0200

    fix: marges Excels

---
+++ src/Controller/ValidationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ValidationController extends AbstractController
+{
+    #[Route('/validation', name: 'app_validation_index')]
+    public function index(): Response
+    {
+        return $this->render('validation/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/validation/liste', name: 'app_validation_formation_liste')]
+    public function liste(): Response
+    {
+        return $this->render('validation/_liste.html.twig', [
+        ]);
+    }
+}
