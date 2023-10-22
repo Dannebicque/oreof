7456389df8137ad78997d2d46c98869e0336700c
davidannebicque <david.annebicque@gmail.com>
2023-10-22 22:31:45 +0200

    feat: validation/composante

--- src/Controller/ValidationController.php
+++ src/Controller/ValidationController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Composante;
+use App\Repository\ComposanteRepository;
+use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ValidationController extends AbstractController
+class ValidationController extends BaseController
 {
     #[Route('/validation', name: 'app_validation_index')]
     public function index(): Response
@@ -25,9 +28,20 @@ class ValidationController extends AbstractController
     }
 
     #[Route('/validation/liste', name: 'app_validation_formation_liste')]
-    public function liste(): Response
-    {
+    public function liste(
+        ComposanteRepository $composanteRepository,
+        FormationRepository  $formationRepository,
+        Request              $request
+    ): Response {
+        $composante = $composanteRepository->find($request->query->get('composante'));
+
+        if (!$composante) {
+            throw $this->createNotFoundException('La composante n\'existe pas');
+        }
+
+        $formations = $formationRepository->findByComposante($composante, $this->getAnneeUniversitaire());
         return $this->render('validation/_liste.html.twig', [
+            'formations' => $formations
         ]);
     }
 }
