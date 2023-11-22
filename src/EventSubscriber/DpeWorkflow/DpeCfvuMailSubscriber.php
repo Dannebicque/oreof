049e3494f63abc30cc8120750d61f220de651e66
davidannebicque <david.annebicque@gmail.com>
2023-11-22 16:17:58 +0100

    feat: Mail validation/refus CFVU

--- src/EventSubscriber/DpeWorkflow/DpeCfvuMailSubscriber.php
+++ src/EventSubscriber/DpeWorkflow/DpeCfvuMailSubscriber.php
@@ -20,7 +20,6 @@ use Symfony\Component\Workflow\Event\Event;
 
 class DpeCfvuMailSubscriber extends AbstractDpeMailSubscriber implements EventSubscriberInterface
 {
-
     public function __construct(
         protected GetHistorique        $getHistorique,
         protected UserRepository       $userRepository,
@@ -33,34 +32,62 @@ class DpeCfvuMailSubscriber extends AbstractDpeMailSubscriber implements EventSu
     public static function getSubscribedEvents(): array
     {
         return [
-            'workflow.dpe.transition.transmettre_cfvu' => 'onTransmettreCfvu',
-            'workflow.dpe.transition.refuser_definitif_cfvu' => 'onRefuseDefinitifCfvu',
-            'workflow.dpe.transition.refuser_revoir_cfvu' => 'onRefuseRevoirCfvu',
-            'workflow.dpe.transition.valider_reserve_cfvu' => 'onValideReserveCfvu',
-            'workflow.dpe.transition.valider_reserve_conseil_cfvu' => 'onValideReserveConseilCfvu',
-            'workflow.dpe.transition.valider_reserve_central_cfvu' => 'onValideReserveCentralCfvu',
+            'workflow.dpe.transition.refuser_definitif_cfvu' => 'onRefuseCfvu',
+            'workflow.dpe.transition.refuser_revoir_cfvu' => 'onRefuseCfvu',
+            'workflow.dpe.transition.valider_reserve_cfvu' => 'onValideCfvu',
+            'workflow.dpe.transition.valider_reserve_conseil_cfvu' => 'onValideCfvu',
+            'workflow.dpe.transition.valider_reserve_central_cfvu' => 'onValideCfvu',
             'workflow.dpe.transition.valider_cfvu' => 'onValideCfvu',
         ];
     }
 
-
-    public function onValideCentral(Event $event)
+    public function onRefuseCfvu(Event $event): void
     {
+        //mail au RF
+        /** @var Formation $formation */
+        $formation = $event->getSubject();
+        $dpe = $formation->getComposantePorteuse()?->getResponsableDpe();
+        //todo: check si le responsable de formation accepte le mail
+        $this->myMailer->initEmail();
+        $this->myMailer->setTemplate(
+            'mails/workflow/formation/refuse_cfvu.html.twig',
+            [
+                'dpe' => $dpe,
+                'formation' => $formation,
+                'context' => $event->getContext(),
+            ]
+        );
+        $this->myMailer->sendMessage(
+            [
+                $dpe?->getEmail(),
+                $formation->getResponsableMention()?->getEmail(),
+                $formation->getCoResponsable()?->getEmail()],
+            '[ORéOF]  Votre formation a été refusée par la CFVU'
+        );
     }
 
-    public function onTransmettreCfvu(Event $event)
+    public function onValideCfvu(Event $event): void
     {
-        //mail aux conseillers ?
-    }
-
-    public function onValideCfvu(Event $event)
-    {
-        //mail aux DPE + RF ?
-    }
-
-    public function onValidePublication(Event $event)
-    {
-        //mail aux DPE + RF ?
-        //tache cron qui scrute la date, publi les éléments et change le marquage de la formation + mail sur event onPublie
+        //mail au RF
+        /** @var Formation $formation */
+        $formation = $event->getSubject();
+        $dpe = $formation->getComposantePorteuse()?->getResponsableDpe();
+        //todo: check si le responsable de formation accepte le mail
+        $this->myMailer->initEmail();
+        $this->myMailer->setTemplate(
+            'mails/workflow/formation/valide_cfvu.html.twig',
+            [
+                'dpe' => $dpe,
+                'formation' => $formation,
+                'context' => $event->getContext(),
+            ]
+        );
+        $this->myMailer->sendMessage(
+            [
+                $dpe?->getEmail(),
+                $formation->getResponsableMention()?->getEmail(),
+                $formation->getCoResponsable()?->getEmail()],
+            '[ORéOF]  Votre formation a été validée par la CFVU'
+        );
     }
 }
