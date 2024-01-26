fa6ee097448a6319f00038a5e50b95be58b1010d
POL MARCHAL <pol.marchal@univ-reims.fr>
2024-01-26 11:49:51 +0100

    Génération des données SOAP au format Excel - Début

--- src/Command/ExportElpApogeeCommand.php
+++ src/Command/ExportElpApogeeCommand.php
@@ -10,13 +10,17 @@ use App\DTO\StructureUe;
 use App\Entity\Parcours;
 use App\Repository\ElementConstitutifRepository;
 use App\Service\Apogee\Classes\ElementPedagogiDTO6;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
 
 #[AsCommand(
     name: 'app:export-elp-apogee',
@@ -27,16 +31,18 @@ class ExportElpApogeeCommand extends Command
 
     private EntityManagerInterface $entityManager;
     private ElementConstitutifRepository $elementConstitutifRepository;
-
+    private Filesystem $filesystem;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        ElementConstitutifRepository $elementConstitutifRepository
+        ElementConstitutifRepository $elementConstitutifRepository,
+        Filesystem $filesystem
     )
     {
         parent::__construct();
         $this->entityManager = $entityManager;
         $this->elementConstitutifRepository = $elementConstitutifRepository;
+        $this->filesystem = $filesystem;
     }
 
     protected function configure(): void
@@ -46,6 +52,10 @@ class ExportElpApogeeCommand extends Command
             mode: InputArgument::OPTIONAL,
             description: 'Execution mode : test or production', 
             default: 'test'
+        )->addArgument(
+            'action', 
+            InputArgument::OPTIONAL,
+            'Génère un export Excel des ELP'
         );
     }
 
@@ -53,18 +63,27 @@ class ExportElpApogeeCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
         $mode = $input->getOption('mode');
+        $action = $input->getArgument('action');
+
 
         if($mode === "test"){
-            $io->success("Début de la commande d'export des ELP");
-            // Récupération de la donnée
+            // retrieve data
             $parcours = $this->entityManager->getRepository(Parcours::class)->findOneById(405);
             $dto = $this->getDTOForParcours($parcours);
-            // Transformation en objet pour le SOAP
+            // transform into valid soap object
             $soapObjectArray = [];
             foreach($dto->semestres[1]->ues() as $ue){
                 $soapObjectArray[] = $this->setObjectForSoapCall($ue, $dto);
             }
-            
+            // excel export
+            if($action === 'excel-export'){
+                $io->writeln("Génération de l'export Excel...");
+                $this->generateSpreadsheetExport($soapObjectArray);
+                $io->success("Fichier généré avec succès.");
+                return Command::SUCCESS;
+            }
+
+            $io->success("Début de la commande d'export des ELP");
 
             dump($soapObjectArray);exit;
 
@@ -92,4 +111,36 @@ class ExportElpApogeeCommand extends Command
         $calculStructure = new CalculStructureParcours($this->entityManager, $this->elementConstitutifRepository);
         return $calculStructure->calcul($parcours);
     }
+
+    private function generateSpreadsheetExport(array $ElpArray){
+        // spreadsheet headers
+        $headers = [
+            "codElp", "libCourtElp", "libElp", "codNatureElp",
+            "codComposante", "temModaliteControle", "nbrCredits",
+            "volume", "uniteVolume", "codPeriode"
+        ];
+        // cast element in array values
+        $ElpArray = array_map(
+            fn($elp) => [
+                $elp->codElp, $elp->libCourtElp, $elp->libElp, 
+                $elp->codNatureElp, $elp->codComposante, $elp->temModaliteControle, 
+                $elp->nbrCredits, $elp->volume, $elp->uniteVolume, $elp->codPeriode
+            ], 
+            $ElpArray
+        );
+        // Write to spreadsheet
+        $spreadsheet = new Spreadsheet();
+        $activeWorksheet = $spreadsheet->getActiveSheet();
+        $activeWorksheet->fromArray($headers);
+        $activeWorksheet->fromArray($ElpArray, startCell: "A2");
+        $activeWorksheet->getPageSetup()->setFitToWidth(1);
+        // Write to file
+        $now = new DateTime();
+        $date = $now->format('d-m-Y_H-i-s');
+        $filename = __DIR__ . "/../Service/Apogee/export/ELP-export-{$date}.xlsx";
+        $this->filesystem->dumpFile($filename, "");
+        $writer = IOFactory::createWriter($spreadsheet, "Xlsx");
+        $writer->save($filename);
+    }
+
 }
