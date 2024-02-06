fd8a6671f4d948b7ce4072dc3575d098869d58d8
davidannebicque <david.annebicque@gmail.com>
2024-02-06 08:49:56 +0100

    feat: validation des fiches niveau SES

--- src/Controller/ValidationController.php
+++ src/Controller/ValidationController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Classes\ValidationProcess;
+use App\Classes\ValidationProcessFicheMatiere;
 use App\Entity\Composante;
 use App\Repository\ComposanteRepository;
+use App\Repository\FicheMatiereRepository;
 use App\Repository\FormationRepository;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -15,24 +17,62 @@ class ValidationController extends BaseController
 {
     #[Route('/validation', name: 'app_validation_index')]
     public function index(
+        Request              $request,
+    ): Response {
+        return $this->render('validation/index.html.twig', [
+            'step' => $request->query->get('step', 'formation'),
+        ]);
+    }
+
+    #[Route('/validation/wizard', name: 'app_validation_wizard')]
+    public function wizard(
+        Request $request,
+        ValidationProcessFicheMatiere    $validationProcessFicheMatiere,
         ValidationProcess    $validationProcess,
         ComposanteRepository $composanteRepository,
     ): Response {
         //todo:  Affichage des bons boutons selon le process et le status choisi sur la liste en bas et haut de page.
-        return $this->render('validation/index.html.twig', [
-            'ses' => true,
-            'composantes' => $composanteRepository->findAll(),
-            'types_validation' => $validationProcess->getProcess(),
-        ]);
+        //todo: filtrer si DPE composante ou pas
+        $idComposante = $request->query->get('composante', null);
+        $step = $request->query->get('step', 'formation');
+        $composante = null;
+        $isSes = $this->isGranted('ROLE_SES');
+
+        if (!$isSes && $idComposante !== null) {
+            $composante = $composanteRepository->find($idComposante);
+        }
+
+        switch ($step) {
+            case 'formation':
+                return $this->render('validation/_formation.html.twig', [
+                    'ses' => $isSes,
+                    'composante' => $composante,
+                    'composantes' => $composanteRepository->findAll(),
+                    'types_validation' => $validationProcess->getProcess(),
+                ]);
+            case 'fiche':
+                return $this->render('validation/_fiches.html.twig', [
+                    'ses' => $isSes,
+                    'composante' => $composante,
+                    'composantes' => $composanteRepository->findAll(),
+                    'types_validation' => $validationProcessFicheMatiere->getProcess(),
+                ]);
+        }
     }
+
+
 
     #[Route('/validation/composante/{composante}', name: 'app_validation_composante_index')]
     public function composante(
+        Request              $request,
         ComposanteRepository $composanteRepository,
         Composante           $composante
     ): Response {
+        $step = $request->query->get('step', 'formation');
+
         return $this->render('validation/index.html.twig', [
-            'composante' => $composante,
+            'step' => $step,
+            'composante' => $composante->getId(),
             'composantes' => $composanteRepository->findAll(),
 
         ]);
@@ -72,6 +112,47 @@ class ValidationController extends BaseController
         return $this->render('validation/_liste.html.twig', [
             'process' => $process,
             'formations' => $formations,
+            'composantes' => $composantes,
+            'etape' => $typeValidation ?? null,
+        ]);
+    }
+
+    #[Route('/validation/liste/fiche', name: 'app_validation_formation_liste_fiche')]
+    public function listeFiche(
+        ValidationProcessFicheMatiere    $validationProcessFicheMatiere,
+        ComposanteRepository $composanteRepository,
+        FicheMatiereRepository  $ficheMatiereRepository,
+        Request              $request
+    ): Response {
+        $typeValidation = $request->query->get('typeValidation');
+        $process = $validationProcessFicheMatiere->getEtape($typeValidation);
+
+        if ($request->query->has('composante')) {
+            if ($request->query->get('composante') === 'all') {
+                $composante = null;
+                $fiches = $ficheMatiereRepository->findByTypeValidation($this->getDpe(), $process['transition']);
+            } else {
+                $composante = $composanteRepository->find($request->query->get('composante'));
+                if (!$composante) {
+                    throw $this->createNotFoundException('La composante n\'existe pas');
+                }
+
+
+                $fiches = $ficheMatiereRepository->findByComposanteTypeValidation($composante, $this->getDpe(), $typeValidation);
+            }
+
+
+
+
+        } else {
+            $fiches = [];
+            $process = null;
+        }
+
+        $composantes = $composanteRepository->findAll();
+        return $this->render('validation/_listeFiches.html.twig', [
+            'process' => $process,
+            'fiches' => $fiches,
             'composantes' => $composantes,
             'etape' => $typeValidation ?? null,
         ]);
