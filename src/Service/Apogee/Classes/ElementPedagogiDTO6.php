6f441044351a1ed281a18b52fd198d67707db2c1
POL MARCHAL <pol.marchal@univ-reims.fr>
2024-02-08 11:18:12 +0100

    Création de contrôles sur les ELP - ElementPedagogiDTO6

--- src/Service/Apogee/Classes/ElementPedagogiDTO6.php
+++ src/Service/Apogee/Classes/ElementPedagogiDTO6.php
@@ -42,25 +42,40 @@ class ElementPedagogiDTO6
     public TableauElementPrerequisDTO2 $listElementPrerequis;
     public TableauTypePopulationDTO2 $listTypePopulation;
 
+    /**
+     * @param StructureEc|StructureUe|StructureSemestre $elementPedagogique Élément à instancier en ELP
+     * @param StructureParcours $dto DTO complet du parcours en question
+     * @param ?CodeNatuElpEnum $nature Nature de l'élément : Semestre (SEM), UE (UE) ou EC (MATI, MATM, ...)
+     * @param bool $withChecks Si la valeur est à true, les données incorrectes lanceront des exceptions
+     */
     public function __construct(
         StructureEc|StructureUe|StructureSemestre $elementPedagogique,
         StructureParcours $dto,
-        ?CodeNatuElpEnum $nature = null
+        ?CodeNatuElpEnum $nature = null,
+        bool $withChecks = false
     ) {
         if ($elementPedagogique instanceof StructureEc){
-            $this->codElp = $elementPedagogique->elementConstitutif->getCodeApogee() ?? "ERROR";
-            $this->libCourtElp = $this->prepareLibelle(
-                $elementPedagogique->elementConstitutif->getFicheMatiere()?->getLibelle() 
-                ?? $elementPedagogique->elementConstitutif->getLibelle(),
-                25
+            // Création du code ELP
+            $this->codElp = $this->checkCodeApogee(
+                $elementPedagogique->elementConstitutif->getCodeApogee(),
+                "Type : EC - ID : {$elementPedagogique->elementConstitutif->getId()}",
+                $withChecks
+            ); 
+            // Libellé court
+            $this->libCourtElp = $this->checkLibelleEC($elementPedagogique, 25, $withChecks);
+            // Libellé long
+            $this->libElp = $this->checkLibelleEC($elementPedagogique, 60, $withChecks);
+            // Nature
+            $this->codNatureElp = $this->checkNatureElp(
+                $nature, 
+                "Type : EC - ID : {$elementPedagogique->elementConstitutif->getId()}",
+                $withChecks
             );
-            $this->libElp = $this->prepareLibelle(
-                $elementPedagogique->elementConstitutif->getFicheMatiere()?->getLibelle()
-                ?? $elementPedagogique->elementConstitutif->getLibelle(), 
-                60
+            $this->codComposante = $this->checkCodeComposante(
+                $dto, 
+                "Type : EC - Parcours ID : {$dto->parcours->getId()}",
+                $withChecks
             );
-            $this->codNatureElp = $nature->value;
-            $this->codComposante = $dto->parcours->getFormation()?->getComposantePorteuse()?->getCodeComposante() ?? "ERROR";
             // $this->temSuspension;
             // $this->motifSuspension;
             $this->temModaliteControle = "O";
@@ -192,11 +207,81 @@ class ElementPedagogiDTO6
         }
     }
 
-    private function prepareLibelle(?string $txt, int $length = 25){
+    private function prepareLibelle(?string $txt, int $length = 25) : string {
         if($txt){
             return mb_substr($txt, 0, $length);
         }else {
             return 'ERROR';
         }
+    }
+
+    /**
+     * Vérifie que le code Apogee pour l'ELP n'est pas null.
+     * @param ?string $codeApogee Code à tester
+     * @param string $identifier Identifiant de l'élément si une erreur survient
+     * @param bool $withChecks Vrai si une exception doit être lancée, renvoie la valeur "ERROR" sinon
+     * @throws \Exception Si $withChecks est à vrai, une exception est lancée
+     * @return string Code APOGEE de l'élément
+     */
+    private function checkCodeApogee(?string $codeApogee, string $identifier, bool $withChecks) : string {
+        return $codeApogee ?? 
+        (
+            $withChecks === false ? "ERROR" 
+            : throw new \Exception("Le code Apogee pour l'élément est incorrect. {$identifier}")
+        );
+    }
+
+    /**
+     * @param StructureEc $elementPedagogique Élément constitutif (matière)
+     * @param int $length Longueur max de la chaîne à renvoyer
+     * @param bool $withChecks Si la valeur est à vrai, une exception est lancée. Renvoie la valeur "ERROR" sinon
+     * @throws \Exception Lance une exception si $withChecks est à vrai
+     * @return string Libellé de l'EC raccourci à la longueur max $length
+     */
+    private function checkLibelleEC(
+        StructureEc $elementPedagogique, 
+        int $length,
+        bool $withChecks = false
+    ) : string {
+        return $elementPedagogique->elementConstitutif->getFicheMatiere()?->getLibelle() 
+            ?? $elementPedagogique->elementConstitutif->getLibelle() ?? ($withChecks === false ?
+            $this->prepareLibelle(
+                $elementPedagogique->elementConstitutif->getFicheMatiere()?->getLibelle() 
+                ?? $elementPedagogique->elementConstitutif->getLibelle(),
+                $length
+        ) : throw new \Exception("Le libellé de l'EC est 'null'. ID : {$elementPedagogique->elementConstitutif->getId()}"));
+    }
+
+    /**
+     * Vérification sur la nature de l'élément pédagogique
+     * @param ?CodeNatuElpEnum Nature de l'élément à tester
+     * @param string $errorIdentifier Identifiant pour le message d'erreur
+     * @param bool $withChecks Si une Exception doit être lancée en cas d'erreur
+     * @throws \Exception Lance une exception si la valeur testée n'est pas correcte et que $withChecks est true
+     * @return ?CodeNatuElpEnum Nature de l'ELP
+     */
+    private function checkNatureElp(?CodeNatuElpEnum $nature, string $errorIdentifier, bool $withChecks) : string {
+        if(($nature === null || $nature instanceof CodeNatuElpEnum === false) && $withChecks){
+            throw new \Exception("La nature de l'ELP est incorrecte. {$errorIdentifier}");
+        }
+        return $nature->value;
+    }
+
+    /**
+     * Vérifie le code composante fourni par le DTO
+     * @param StructureParcours DTO du parcours
+     * @param string $errorIdentifier Indication pour le message d'erreur
+     * @param bool $withChecks Si une exception doit être lancée en cas d'erreur
+     * @throws \Exception Si $withChecks est à vrai, lance une exception
+     * @return string Code composante du parcours
+     */
+    private function checkCodeComposante(StructureParcours $dto, string $errorIdentifier, bool $withChecks) : string {
+        return $dto->parcours->getFormation()?->getComposantePorteuse()?->getCodeComposante() 
+        ?? 
+        (
+            $withChecks === false 
+            ? "ERROR"
+            : throw new \Exception("Le code de la composante porteuse est 'null'. {$errorIdentifier}")
+        );
     }
 }
