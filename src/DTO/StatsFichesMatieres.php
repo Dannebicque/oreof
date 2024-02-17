7bdecb752ffabc43e6154b4bcab98ac580d8530c
davidannebicque <david.annebicque@gmail.com>
2024-02-17 15:41:39 +0100

    fix: stats sur les fiches

---
+++ src/DTO/StatsFichesMatieres.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DTO;
+
+/*
+ * Copyright (c) 2023. | David Annebicque | ORéOF  - All Rights Reserved
+ * @file /Users/davidannebicque/Sites/oreof/src/DTO/StatsFichesMatieresParcours.php
+ * @author davidannebicque
+ * @project oreof
+ * @lastUpdate 07/12/2023 11:42
+ */
+
+
+use App\Entity\ElementConstitutif;
+use App\Entity\FicheMatiere;
+use App\Entity\Parcours;
+
+class StatsFichesMatieres
+{
+    public int $nbFiches = 0;
+    public int $nbFichesValidees = 0; //par central
+    public int $nbFichesNonValideesSes = 0;
+    public int $nbFichesCompletes = 0;
+    public int $nbFichesNonValidees = 0;
+
+    public int $nbFichesPubliees = 0;
+
+    public function addStatsParcours(StatsFichesMatieresParcours $statsFichesMatieresParcours): void
+    {
+        $this->nbFiches += $statsFichesMatieresParcours->nbFiches;
+        $this->nbFichesValidees += $statsFichesMatieresParcours->nbFichesValidees;
+        $this->nbFichesNonValideesSes += $statsFichesMatieresParcours->nbFichesNonValideesSes;
+        $this->nbFichesCompletes += $statsFichesMatieresParcours->nbFichesCompletes;
+        $this->nbFichesNonValidees += $statsFichesMatieresParcours->nbFichesNonValidees;
+        $this->nbFichesPubliees += $statsFichesMatieresParcours->nbFichesPubliees;
+    }
+}
