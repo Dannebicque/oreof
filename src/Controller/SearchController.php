0809d1e9fe80bcb70ce53d86067340229e5cb3e3
POL MARCHAL <pol.marchal@univ-reims.fr>
2024-07-11 11:13:14 +0200

    Route for search tool - SearchController

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class SearchController extends AbstractController
+{
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/search', name: 'app_search')]
+    public function index(): Response
+    {
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+}
