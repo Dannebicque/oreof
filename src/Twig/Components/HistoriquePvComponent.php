bbf47e3e22c7aa788ca3785a3f57b29f16475745
davidannebicque <david.annebicque@gmail.com>
2024-09-06 16:49:12 +0200

    fix: PV déposé/laisser passer

--- src/Twig/Components/HistoriquePvComponent.php
+++ src/Twig/Components/HistoriquePvComponent.php
@@ -2,6 +2,8 @@
 
 namespace App\Twig\Components;
 
+use App\Classes\GetDpeParcours;
+use App\Classes\GetHistorique;
 use App\Entity\Parcours;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 use Symfony\UX\TwigComponent\Attribute\PostMount;
@@ -13,19 +15,28 @@ final class HistoriquePvComponent
     public bool $hasPv = false;
     public ?string $fichier = null;
 
+    public function __construct(private GetHistorique $getHistorique)
+    {
+    }
+
     #[PostMount]
-    public function postMount() {
+    public function postMount()
+    {
         if ($this->parcours === null) {
             return;
         }
-        $historique = $this->parcours->getHistoriqueParcours();
-        foreach ($historique as $h) {
-           if ($h->getEtape() === 'soumis_conseil' || $h->getEtape() === 'conseil') {//état conseil avant, les deux pour compatibilité
-               if (array_key_exists('fichier', $h->getComplements())) {
-                     $this->hasPv = true;
-                     $this->fichier = $h->getComplements()['fichier'];
-               }
-           }
+        $dpe = GetDpeParcours::getFromParcours($this->parcours);
+        if ($dpe === null) {
+            return;
+        }
+
+        $historique = $this->getHistorique->getHistoriqueParcoursLastStep($dpe, 'soumis_conseil');
+
+        if ($historique !== null) {//état conseil avant, les deux pour compatibilité
+            if (array_key_exists('fichier', $historique->getComplements())) {
+                $this->hasPv = true;
+                $this->fichier = $historique->getComplements()['fichier'];
+            }
         }
     }
 }
