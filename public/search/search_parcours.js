7c877feb73892028eafbf7336b5faa8d135149e1
davidannebicque <david.annebicque@gmail.com>
2024-11-23 12:49:54 +0100

    fix: target blank sur la recherche

--- public/search/search_parcours.js
+++ public/search/search_parcours.js
@@ -1,63 +1,64 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-    let ficheMatiereUrl = document.querySelector('#rootListElement').getAttribute('data-fiche-matiere-url');
-    
-    let links = document.querySelectorAll('.fiche-matiere-search-link');
+  const ficheMatiereUrl = document.querySelector('#rootListElement').getAttribute('data-fiche-matiere-url');
 
-    links.forEach(link => {
-        link.addEventListener('click', (event) => {
-            let target = event.target;
-            let fetchUrl = target.getAttribute('data-fetch-url');
-            let modalTitleElement = document.querySelector('#titre-modale-recherche');
+  const links = document.querySelectorAll('.fiche-matiere-search-link');
 
-            modalTitleElement.innerHTML = "";
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      const { target } = event;
+      const fetchUrl = target.getAttribute('data-fetch-url');
+      const modalTitleElement = document.querySelector('#titre-modale-recherche');
 
-            let listNode = document.querySelector('#associated-fiche-matiere-modal-body')
-            // Empty node list
-            while(listNode.hasChildNodes()){
-                listNode.removeChild(listNode.firstChild);
-            }
+      modalTitleElement.innerHTML = '';
 
-            // loading icon
-            let loadingIcon = document.createElement('i');
-            loadingIcon.className = "fa-duotone fa-spinner spinning-icon";
-            listNode.appendChild(loadingIcon);
+      const listNode = document.querySelector('#associated-fiche-matiere-modal-body')
+      // Empty node list
+      while (listNode.hasChildNodes()) {
+        listNode.removeChild(listNode.firstChild);
+      }
 
-            // fetching result
-            fetch(fetchUrl)
-                .then(response => response.json())
-                .then(jsonArray => {
-                    loadingIcon.remove();
+      // loading icon
+      const loadingIcon = document.createElement('i');
+      loadingIcon.className = 'fa-duotone fa-spinner spinning-icon';
+      listNode.appendChild(loadingIcon);
 
-                    let fichesNumber = Number(target.getAttribute('data-number-associated-fiches-matieres'));
-                    if(fichesNumber >= 2){
-                        modalTitleElement.innerHTML = `${fichesNumber} fiches matières associées`;
-                    }
-                    else if(fichesNumber === 1) {
-                        modalTitleElement.innerHTML = '1 fiche matière associée'
-                    }
+      // fetching result
+      fetch(fetchUrl)
+        .then((response) => response.json())
+        .then((jsonArray) => {
+          loadingIcon.remove();
 
-                    let listParent = document.createElement('ul');
-                    jsonArray.forEach(ficheMatiere => {
-                        let listElement = document.createElement('li');
-                        listElement.className = "my-2"
+          const fichesNumber = Number(target.getAttribute('data-number-associated-fiches-matieres'));
+          if (fichesNumber >= 2) {
+            modalTitleElement.innerHTML = `${fichesNumber} fiches matières associées`;
+          } else if (fichesNumber === 1) {
+            modalTitleElement.innerHTML = '1 fiche matière associée'
+          }
 
-                        let link = document.createElement('a');
-                        link.innerHTML = ficheMatiere.libelle;
-                        link.href = ficheMatiereUrl.replace('%C2%B5%23+', ficheMatiere.slug);
+          const listParent = document.createElement('ul');
+          jsonArray.forEach((ficheMatiere) => {
+            const listElement = document.createElement('li');
+            listElement.className = 'my-2'
 
-                        listElement.appendChild(link);
-                        listParent.appendChild(listElement);
-                    });
-                    listNode.appendChild(listParent);
-                })
-            // handling AJAX error
-            .catch(error => {
-                loadingIcon.remove();
-                textErrorModal = document.createElement('h3');
-                textErrorModal.innerHTML = "Un problème est survenu lors de la récupération des données.";
-                textErrorModal.className = 'text-primary text-center';
-                listNode.appendChild(textErrorModal);
-            });
+            const link = document.createElement('a');
+            // link.addAttribute('target', '_blank');
+            link.innerHTML = ficheMatiere.libelle;
+            link.href = ficheMatiereUrl.replace('%C2%B5%23+', ficheMatiere.slug);
+            link.target = '_blank';
+
+            listElement.appendChild(link);
+            listParent.appendChild(listElement);
+          });
+          listNode.appendChild(listParent);
+        })
+      // handling AJAX error
+        .catch((error) => {
+          loadingIcon.remove();
+          textErrorModal = document.createElement('h3');
+          textErrorModal.innerHTML = 'Un problème est survenu lors de la récupération des données.';
+          textErrorModal.className = 'text-primary text-center';
+          listNode.appendChild(textErrorModal);
         });
-    })
+    });
+  })
 });
