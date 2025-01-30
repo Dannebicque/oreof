7fc62dc04973d102e1f20d651f86e0c35114f231
POL MARCHAL <pol.marchal@univ-reims.fr>
2025-01-30 13:50:12 +0100

    Création de la commande 'NewAnneeUniversitaire"

---
+++ src/Command/NewAnneeUniversitaireCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:new-annee-universitaire',
+    description: 'Duplique tous les parcours et formations, pour créer une nouvelle année universitaire',
+)]
+class NewAnneeUniversitaireCommand extends Command
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    )
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure(): void
+    {
+        $this->addOption(
+            name: 'generate-full-database', 
+            mode: InputOption::VALUE_NONE, 
+            description: 'Copie tous les parcours disponibles'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        
+        $generateFullDatabase = $input->getOption('generate-full-database');
+
+        if($generateFullDatabase){
+            $io->writeln("\nCommande pour copier les parcours et formations sur une nouvelle année universitaire.\n");
+            $io->writeln("La commande va copier l'année actuelle pour créer l'année 2025-2026\n");
+            if($io->ask("Souhaitez-vous poursuivre ? [Y/n]", 'n') === "Y"){
+
+                return Command::SUCCESS;
+            }
+            
+            $io->writeln("Commande annulée");
+            return Command::SUCCESS;
+        }
+
+        $io->warning("Option non reconnue. Doit être parmi ['generate-full-database']");
+        return Command::SUCCESS;
+    }
+}
