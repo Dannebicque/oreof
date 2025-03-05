c9eb0de471a5f032090de30d50f199066cba83c2
davidannebicque <david.annebicque@gmail.com>
2025-03-05 09:09:02 +0100

    fix: Export fiches

---
+++ src/Classes/Export/ExportListeFicheMatiere.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Copyright (c) 2023. | David Annebicque | ORéOF  - All Rights Reserved
+ * @file /Users/davidannebicque/Sites/oreof/src/Classes/Export/ExportSynthese.php
+ * @author davidannebicque
+ * @project oreof
+ * @lastUpdate 11/11/2023 13:07
+ */
+
+namespace App\Classes\Export;
+
+use App\Classes\Excel\ExcelWriter;
+use App\Classes\GetHistorique;
+use App\Entity\CampagneCollecte;
+use App\Entity\ElementConstitutif;
+use App\Repository\ElementConstitutifRepository;
+use App\Repository\FicheMatiereRepository;
+use App\Utils\Tools;
+use DateTime;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class ExportListeFicheMatiere implements ExportInterface
+{
+    private string $fileName;
+    private string $dir;
+
+    public function __construct(
+        protected GetHistorique        $getHistorique,
+        protected ExcelWriter         $excelWriter,
+        KernelInterface               $kernel,
+        protected FicheMatiereRepository $ficheMatiereRepository,
+    ) {
+        $this->dir = $kernel->getProjectDir() . '/public/temp/';
+    }
+
+    private function prepareExport(
+        CampagneCollecte $anneeUniversitaire,
+    ): void {
+        $fiches = $this->ficheMatiereRepository->findBy([
+            'campagneCollecte' => $anneeUniversitaire,
+        ], [
+            'libelle' => 'ASC'
+        ]);
+        $this->excelWriter->nouveauFichier('Export Fiches Matieres');
+        $this->excelWriter->setActiveSheetIndex(0);
+
+        $this->excelWriter->writeCellXY(1, 1, 'Id');
+        $this->excelWriter->writeCellXY(2, 1, 'Fiche EC/matière');
+        $this->excelWriter->writeCellXY(3, 1, 'Référent');
+        $this->excelWriter->writeCellXY(4, 1, 'Complet ?');
+        $this->excelWriter->writeCellXY(5, 1, 'Utilisée ?');
+        $this->excelWriter->writeCellXY(6, 1, 'Parcours porteur');
+
+        $ligne = 2;
+        /** @var ElementConstitutif $ec */
+        foreach ($fiches as $fiche) {
+            $this->excelWriter->writeCellXY(1, $ligne, $fiche->getId());
+            $this->excelWriter->writeCellXY(2, $ligne, $fiche->getLibelle());
+            $this->excelWriter->writeCellXY(3, $ligne, $fiche->getResponsableFicheMatiere() !== null ? $fiche->getResponsableFicheMatiere()->getDisplay() : '');
+            $this->excelWriter->writeCellXY(4, $ligne, $fiche->remplissageBrut()->isFull() ? 'Complet' : 'Incomplet');
+            $this->excelWriter->writeCellXY(5, $ligne, $fiche->getElementConstitutifs()->count() > 0 ? $fiche->getElementConstitutifs()->count() : 0);
+            $this->excelWriter->writeCellXY(6, $ligne,
+            $fiche->isHorsDiplome() === true ? 'Hors diplôme' : ($fiche->getParcoursPorteur() !== null ? $fiche->getParcoursPorteur()->getLibelle() : ''
+            ));
+
+            $this->excelWriter->getColumnsAutoSize('A', 'M');
+            $ligne++;
+        }
+
+        $this->fileName = Tools::FileName('Export - Fiches Matières - ' . (new DateTime())->format('d-m-Y-H-i'), 30);
+    }
+
+    public function export(CampagneCollecte $anneeUniversitaire): StreamedResponse
+    {
+        $this->prepareExport($anneeUniversitaire);
+        return $this->excelWriter->genereFichier($this->fileName);
+    }
+
+    public function exportLink(CampagneCollecte $campagneCollecte): string
+    {
+        $this->prepareExport($campagneCollecte);
+        $this->excelWriter->saveFichier($this->fileName, $this->dir . 'zip/');
+        return $this->fileName . '.xlsx';
+    }
+}
