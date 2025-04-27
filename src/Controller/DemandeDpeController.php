f431ca9b37a798d2f939d8413865a7267636e2bc
davidannebicque <david.annebicque@gmail.com>
2025-04-27 11:39:08 +0200

    feat: Add export functionality and update access controls
    Introduced an Excel export feature for DPE requests and updated breadcrumbs. Revised access restrictions, enforcing ROLE_ADMIN permissions for certain actions and removing DPE delete functionality from the UI.

--- src/Controller/DemandeDpeController.php
+++ src/Controller/DemandeDpeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classes\Excel\ExcelWriter;
 use App\Classes\JsonReponse;
 use App\Entity\Composante;
 use App\Entity\DpeDemande;
@@ -18,12 +19,12 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class DemandeDpeController extends AbstractController
 {
     #[Route('/demande/dpe', name: 'app_demande_dpe')]
-    #[IsGranted('ROLE_SES')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(
     ): Response
     {
         return $this->render('demande_dpe/index.html.twig', [
-            'type' => 'ses',
+            'type' => 'ses'
         ]);
     }
 
@@ -46,8 +47,10 @@ class DemandeDpeController extends AbstractController
             return $this->render('demande_dpe/_liste.html.twig', [
                 'demandes' => $dpeDemandeRepository->findByComposante($composante),
             ]);
-        } elseif ($type === 'ses') {
-            $this->denyAccessUnlessGranted('ROLE_SES');
+        }
+
+        if ($type === 'ses') {
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
             return $this->render('demande_dpe/_liste.html.twig', [
                 'demandes' => $dpeDemandeRepository->findAll(),
@@ -70,22 +73,67 @@ class DemandeDpeController extends AbstractController
         ]);
     }
 
-    #[Route('/demande-dpe/{id}', name: 'app_dpe_demande_delete', methods: ['DELETE'])]
-    public function delete(
-        EntityManagerInterface $entityManager,
-        DpeDemande $dpeDemande,
+    #[Route('/demande/dpe/export/{type}', name: 'app_demande_dpe_export')]
+    public function dpeExport(
+        ExcelWriter          $excelWriter,
+        DpeDemandeRepository $dpeDemandeRepository,
+        ComposanteRepository $composanteRepository,
         Request $request,
+        string               $type,
     ): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$dpeDemande->getId(), JsonRequest::getValueFromRequest($request, 'csrf'))) {
-            $entityManager->remove($dpeDemande);
-            $entityManager->flush();
-            return JsonReponse::success('La demande a bien été supprimée.');
+        if ($type === 'composante') {
+            $composante = $composanteRepository->find($request->query->get('composante'));
+            if ($composante === null) {
+                throw $this->createNotFoundException('Composante non trouvée');
+            }
 
+            $this->denyAccessUnlessGranted('CAN_COMPOSANTE_SHOW_MY', $composante);
+
+            $demandes = $dpeDemandeRepository->findByComposante($composante);
+        } else {
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+            $demandes = $dpeDemandeRepository->findAll();
         }
 
-        return JsonReponse::error('Erreur lors de la suppression de la demande.');
-    }
+        $filename = 'demandes_dpe_' . date('Y-m-d_H-i-s') . '.xlsx';
+        $excelWriter->nouveauFichier('Export Demande DPE');
+        $excelWriter->setActiveSheetIndex(0);
+        $excelWriter->writeCellName('A1', 'Composante');
+        $excelWriter->writeCellName('B1', 'Mention');
+        $excelWriter->writeCellName('C1', 'Parcours');
+        $excelWriter->writeCellName('D1', 'Demande de ?');
+        $excelWriter->writeCellName('E1', 'Date demande');
+        $excelWriter->writeCellName('F1', 'Date clôture');
+        $excelWriter->writeCellName('G1', 'Niveau demande');
+        $excelWriter->writeCellName('H1', 'Etat');
+        $excelWriter->writeCellName('I1', 'Commentaire');
+        $ligne = 2;
+        foreach ($demandes as $demande) {
+            $parcours = $demande->getParcours();
+            $formation = $parcours->getFormation();
+            $composante = $formation->getComposantePorteuse();
+            $excelWriter->writeCellName('A' . $ligne, $composante->getLibelle());
+            $excelWriter->writeCellName('B' . $ligne, $formation->getDisplay());
 
-    //si acceptation ajouter à l'historique, mail DPE +RF? RP? + changement état workflow. Gérer workflow avec ou sans SES
+            if ($demande->getNiveauDemande() === 'F') {
+                $excelWriter->writeCellName('C' . $ligne, 'Niveau Mention');
+            } else {
+                $excelWriter->writeCellName('C' . $ligne, $parcours->getDisplay());
+            }
+
+            $excelWriter->writeCellName('D' . $ligne, $demande->getAuteur() ? $demande->getAuteur()->getDisplay() : '');
+            $excelWriter->writeCellName('E' . $ligne, $demande->getDateDemande()?->format('d/m/Y'));
+            $excelWriter->writeCellName('F' . $ligne, $demande->getDateCloture() ? $demande->getDateCloture()->format('d/m/Y') : '');
+            $excelWriter->writeCellName('G' . $ligne, $demande->getNiveauModification() ? $demande->getNiveauModification()->getLibelle() : '');
+            $excelWriter->writeCellName('H' . $ligne, $demande->getEtatDemande()?->getLibelle());
+            $excelWriter->writeCellName('I' . $ligne, $demande->getArgumentaireDemande());
+            $ligne++;
+        }
+
+        $excelWriter->getColumnsAutoSize('A', 'I');
+
+        return $excelWriter->genereFichier($filename);
+    }
 }
