d5b9795dd8175bcf9b6edac1f40b94fca003a57e
davidannebicque <david.annebicque@gmail.com>
2026-03-30 16:22:13 +0200

    feat: enhance administration routes and improve comment handling in controllers

--- src/Controller/Config/DomaineController.php
+++ src/Controller/Config/DomaineController.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/domaine')]
+#[Route('/administration/domaine')]
 class DomaineController extends AbstractController
 {
     #[Route('/', name: 'app_domaine_index', methods: ['GET'])]
